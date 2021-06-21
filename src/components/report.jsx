bd24fd9ae53d2d347b73685320796a2d70555020
MuhammadQasim97 <qasimnazir99@gmail.com>
2021-06-21 11:29:45 +0500

    Update report.jsx

--- src/components/report.jsx
+++ src/components/report.jsx
@@ -1,24 +1,71 @@
 import { render } from '@testing-library/react';
-import React, { Component } from 'react';
-
+import React, { Component,useEffect,useState } from 'react';
+import {Link, useLocation} from "react-router-dom";
+import axios from "axios";
 const Report = (props)=>{
+  let location = useLocation();
+  let [score,setScore]=useState(0);
+  let [loc,lineOfCode]=useState(0);
 
-    console.log(props);
-    return(
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log(location.state);
+    let id=location.state;
 
-      <h1>HEllo</h1>
-    );
-}
 
-// class Report extends Component{
+        
+        axios({
+          method: "get",
+          url: "http://localhost:5000/getReport/" + id,
+          config: { headers: { "Content-Type": "multipart/form-data" } },
+        })
+          .then((response) => {
+            if (response.status == 200) {
+            }
+            console.log(response.data.data[0]);
+            setScore(response.data.data[0][11]);
+            lineOfCode(response.data.data[0][9]);
+
+          })
+          .catch((errors) => console.log(errors));
+        
+  });
+
+  // componentWillMount = (event, params)=> {
+  //   //props.setView();
+  //   console.log(props);
+  //   if (
+  //     event.type === "keydown" &&
+  //     (event.key === "Tab" || event.key === "Shift")
+  //   ) {
+  //     alert("YEAH");
+  //   } else {
+  //     let id = params.getValue("id");
+  //     axios({
+  //       method: "get",
+  //       url: "http://localhost:5000/getsubmission/" + id,
+  //       config: { headers: { "Content-Type": "multipart/form-data" } },
+  //     })
+  //       .then((response) => {
+  //         if (response.status == 200) {
+  //         }
+  //         console.log(response.status);
+  //       })
+  //       .catch((errors) => console.log(errors));
+  //   }
+  // }
 
 
     
-//     render (){
-//         console.log(this.props);
-//         return (<h1>{this.props.value}Hello</h1>);
-//     }
-// }
+    return(
+
+      <div>
+            <h1>Score is {score}</h1>
+            <h1>Line of Codes are {loc}</h1>
+      </div>
+    );
+}
+
 
 
 export default Report;
